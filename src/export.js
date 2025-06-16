a987ea54bacf6bdf8cfb852308b0081d183f7fba
godz26 <xfirenic@gmail.com>
2025-06-16 11:50:22 +0800

    fix code

---
+++ src/export.js
@@ -0,0 +1,45 @@
+import building from "../assets/building.svg";
+import construction from "../assets/construction.svg";
+import design from "../assets/design.svg";
+import document from "../assets/document.svg";
+import paint from "../assets/paint.svg";
+import support from "../assets/support.svg";
+
+export const allservices = [
+  {
+    icon: building,
+    title: "BUILDING RENOVATION",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: construction,
+    title: "CONSTRUCTION SERVICES",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: design,
+    title: "DESIGN & PLANNING",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: document,
+    title: "DOCUMENTATION",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: paint,
+    title: "INTERIOR DESIGN",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: support,
+    title: "CUSTOMER SUPPORT",
+    about:
+      "Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
